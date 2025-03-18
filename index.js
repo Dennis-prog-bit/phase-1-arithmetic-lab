efaabfb19391341255e93e444a3e67e63a8001e0
Dennis Kyalo <kyalodennis1738@gmail.com>
2025-03-18 20:27:23 +0300

    final

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+// equation whose result equals 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 31 * 2 = 62
+
+//random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1; // Ensures a positive integer
+
+// equation that calculates a remainder equal to 4
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4; // 24 % 5 = 4
+
+//will return 20 as the highest number in the set
+const max = Math.max(1, 5, 10, 15, 20);
